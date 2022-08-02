83f0b2e588437afd7aef496e840fcc2f962be178
dhruvked <dhruvkedia2@gmail.com>
2022-08-02 22:07:03 +0530

    start

--- monsters-roladex/src/App.js
+++ monsters-roladex/src/App.js
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          My Name is Dhruv
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button>Change Name</button>
       </header>
     </div>
   );
