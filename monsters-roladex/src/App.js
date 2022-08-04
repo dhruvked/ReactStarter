50ed66d47d7ae185670d96b3f5a2f65b6a74fdb6
dhruvked <dhruvkedia2@gmail.com>
2022-08-04 23:18:22 +0530

    added search bar

--- monsters-roladex/src/App.js
+++ monsters-roladex/src/App.js
@@ -6,22 +6,37 @@ class App extends Component{
   constructor(){
     super();
     this.state={
-      monsters:[
-        {
-          name:'Dhruv',
-          id:'143r2345'
-        },
-        {
-          name:'Kedia',
-          id:'245ewwe'
-        }
-      ]
+      monsters:[],
+      search: []
     }
   }
+
+  componentDidMount(){
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(Response=>Response.json())
+    .then(users=>this.setState(()=>{
+      return {
+              monsters: users,
+              search: users
+             }
+    }))
+  }
+
   render(){
     return (
     <div className="App">
-      {this.state.monsters.map((monster)=>{
+      <input className='search-box' type="search" placeholder='Search Monsters' onChange={(event) => {
+        let temp="";
+        if(event.target.value===""){
+          temp=this.state.monsters;
+        }
+        else{
+          temp=this.state.monsters.filter((monster)=>monster.name.toLowerCase().includes(event.target.value.toLowerCase()));
+        }
+        console.log(this.state.search)
+        this.setState(()=>{return {search: temp}});
+      }}/>
+      {this.state.search.map((monster)=>{
         return <h1 key={monster.id}>{monster.name}</h1>
       })}
     </div>
